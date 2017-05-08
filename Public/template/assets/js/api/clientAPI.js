d26b926e4bf64084fc80024ab7cd764a070c40f6
Deeeeeee <445214770@qq.com>
2017-05-08 23:23:13 +0800

    up

--- Public/template/assets/js/api/clientAPI.js
+++ Public/template/assets/js/api/clientAPI.js
@@ -4,11 +4,11 @@
 
 define(["jquery"], function ($) {
     var clientAPI = {
+
+        baseRequestUrl:"..",
         /**
          * 客户管理-修改额度
          */
-        baseRequestUrl:"..",
-
         changeLine: function (data,cb) {
             $.post(this.baseRequestUrl+"/addOrg.php",data,function (result) {
                 cb(result);
@@ -59,6 +59,34 @@ define(["jquery"], function ($) {
          * 客户管理-客户列表-入金列表
          */
         getInMoneyList: function (data,cb) {
+            $.post(this.baseRequestUrl+"/trade/keep",data,function (result) {
+                cb(result);
+            })
+        },
+
+
+
+
+        /**
+         * 客户管理-持仓查询-列表
+         */
+        getCCList: function (data,cb) {
+            $.post(this.baseRequestUrl+"/trade/keep",data,function (result) {
+                cb(result);
+            })
+        },
+        /**
+         * 客户管理-平仓查询-列表
+         */
+        getPCList: function (data,cb) {
+            $.post(this.baseRequestUrl+"/trade/keep",data,function (result) {
+                cb(result);
+            })
+        },
+        /**
+         * 客户管理-出入金查询-列表
+         */
+        getCRJList: function (data,cb) {
             $.post(this.baseRequestUrl+"/trade/keep",data,function (result) {
                 cb(result);
             })
